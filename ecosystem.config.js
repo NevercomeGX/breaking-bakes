731e28d7c0b6606c05a44fce3d12e503dc60f0b4
NevercomeG <luisjavier2012@gmail.com>
2022-12-17 18:45:52 -0400

    2.1.3

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,32 @@
 module.exports = {
-    apps: [
-      {
-        name: "bakes",
-        script: "next",
-        args: "-p 3001"
-      }
-    ]
-  }
+  apps: [
+    {
+      name: "portfolio",
+      script: "yarn",
+      args: "start",
+      watch: true,
+      env: {
+        NODE_ENV: "development",
+        PORT: 3000,
+      },
+      env_production: {
+        NODE_ENV: "production",
+        PORT: 3000,
+      },
+    },
+    {
+      name: "bakes",
+      script: "yarn",
+      args: "start",
+      watch: true,
+      env: {
+        NODE_ENV: "development",
+        PORT: 3001,
+      },
+      env_production: {
+        NODE_ENV: "production",
+        PORT: 3001,
+      },
+    },
+  ],
+};
